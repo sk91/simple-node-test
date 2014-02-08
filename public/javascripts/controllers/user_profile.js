0044b525ef7ed5ea46872dbbe2baf2e1bd169b51
Maxim Kovalov <max.kovalov@gmail.com>
2014-02-08 17:02:43 +0200

    Profile controller works

--- public/javascripts/controllers/user_profile.js
+++ public/javascripts/controllers/user_profile.js
@@ -1,8 +1,11 @@
 'use strict';
 
 angular.module('embraase')
-  .controller('UserProfileCtrl', function ($scope,$http) {
+  .controller('UserProfileCtrl', function ($scope,$http,$location) {
     $scope.user = $http.get('session').success(function(data){
+        if('logout' in data){
+          return $location.path('/login');
+        }
         $scope.user = data;
     });
   });
