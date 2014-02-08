7b2807867e7a7ad2a4f3474bb630b349796d4520
Maxim Kovalov <max.kovalov@gmail.com>
2014-02-08 14:40:00 +0200

    user auth

--- models/user.js
+++ models/user.js
@@ -1,4 +1,5 @@
-var _= require("lodash");
+var _= require("lodash")
+  , bcrypt = require('bcrypt');
 
 var User = module.expots = {
 
@@ -12,16 +13,58 @@ var User = module.expots = {
       this._db.users.findOne(params,cb);
     },
 
-    save:function(user,cb){
-      this.save(user,cb);
+    create: function(user,cb){
+      this.validate(user,validateCb.bind(this));
+
+      function validateCb(err){
+        if(err) return cb(err);
+
+        this.createHash(user.password,hashCreatedCb.bind(this));  
+      }
+
+      function hashCreatedCb(err,password){
+        if(err) return cb(err);
+
+        user.password = password;
+        this._db.users.save(user,userCreatedCb.bind(this));
+      }
+
+      function userCreatedCb(err, user){
+        if(err) return cb(err);
+
+        if(!user){
+          return cb("not found");
+        }
+
+        return cb(null, user);
+      }
+
     },
 
-    validate:function(user){
-      return true;
+
+    validate:function(user, cb){
+      if(!user.email || user.email.indexOf('@') < 0){
+        return cb("invalid email");
+      }
+
+      if(!user.password){
+        return cb('invalid password'));
+      }
+
+      return cb(null,user);
     },
 
-    validatePassword: function(user, password){
+    
 
+    validatePassword: function(user, password,cb){
+      bcrypt.compare(password, user.password, cb);
+    },
+
+    createHash:function(password, cb){
+      bcrypt.hash(password, 5 , cb);
     }
+
+
+
 
 }
